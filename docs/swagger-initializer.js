97c6800bed8a3ac45c762a8c5bec7d1711be8d7c
홍성문 <2020041412@hanyang.ac.kr>
2025-05-01 18:50:39 +0900

    swagger test

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://github.com/SWYP-9TH-FITEAM/Fiteam_BE/tree/main/docs/swagger.json",
+    url: "https://github.com/SWYP-9TH-FITEAM/Fiteam_BE/blob/main/docs/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
